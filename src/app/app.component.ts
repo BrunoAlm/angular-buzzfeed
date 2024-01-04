585ff9280c7244693f6829af8afea82fd9331948
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2024-01-04 17:53:25 -0300

    add initial structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet, HomeComponent],
+  template: `
+  <app-home></app-home>
+  <router-outlet></router-outlet>
+`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
